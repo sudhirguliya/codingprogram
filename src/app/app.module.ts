0aed31b95d721bfb6f1913aa8d84626cb5bb5c57
sudhir <sudhir.se11@gmail.com>
2018-05-03 07:55:32 +0530

    R&D route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 //Routing
-import { routing } from './app.routes';
-import { Routes , Router, RouterModule } from '@angular/router';
+//import { routing } from './app.routes';
+import { Route, Routes , Router, RouterModule } from '@angular/router';
 //Service
 import { AppGlobals } from './app.global.service';
 import { RouterService } from './_services/router.service'
@@ -16,12 +16,12 @@ import { HomeLayoutComponent } from './layouts/home-layout.component';
 //Component
 import { NotFoundComponent } from './not-found/not-found.component';
 
-/*let appRoutes = [
+var appRoutes: Route[] = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeLayoutComponent },
   //{ path: 'coding', component: HomeLayoutComponent },
   { path: '**',     component: NotFoundComponent }
-];*/
+];
 
 @NgModule({
   declarations: [
@@ -29,11 +29,38 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule, HttpModule, FormsModule, 
-    //RouterModule.forRoot(appRoutes),
-    routing,
+    RouterModule.forRoot(appRoutes)
+    //routing,
   ],
   providers: [AppGlobals,RouterService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor(private router:Router, private routerService:RouterService) {
+    // This works
+      // Pushing the same route as in routes.json
+      // ----------------------
+      /*let routes = this.router.config;
+      routes.push({ path: 'contact', component: HomeLayoutComponent })
+      this.router.resetConfig(routes);*/
+
+      /*let r: Route = {
+          path: 'pop',
+          component: HomeLayoutComponent
+      };
+      this.router.resetConfig([r, ...this.router.config]);*/
+
+      // This does works
+      // By just console log this anonymous object, the loaded routing from the 
+      // json data will work. 
+      // ----------------------
+      //console.log({ loadChildren: './contact/contact.module#ContactModule' })
+      this.routerService.getCategories().subscribe((result) => { 
+        result[0].category_link.forEach((route) => { console.log(route);
+          appRoutes.push({ path: route, component: HomeLayoutComponent })
+        });
+        console.log(appRoutes);
+        this.router.resetConfig(appRoutes);
+      });
+  }
 }
